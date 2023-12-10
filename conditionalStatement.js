e132e4df171ce0c2dfcc4602888e87796aceab91
Mausam Dahal <104712032+Mausam1221@users.noreply.github.com>
2023-12-10 21:30:52 +0545

    Create conditionalStatement.js

---
+++ conditionalStatement.js
@@ -0,0 +1,28 @@
+et grade=prompt("enter score:");
+if(grade>=90 &&grade<=100){
+    console.log("A+");
+}
+else if(grade>=80 && grade<=89){
+    console.log("A");
+}
+else if(grade>=70 && grade<=79){
+    console.log("B+");
+}
+else if(grade>=60 && grade<=69){
+    console.log("B");
+}
+else if(grade>=50 && grade<=59){
+    console.log("B");
+}
+else if(grade>=40 && grade<=49){
+    console.log("C+");
+}
+else if(grade>=30 && grade<=39){
+    console.log("C");
+}
+if(grade>=20 && grade<=29){
+    console.log("C");
+}
+else{
+    console.log("F")
+}
